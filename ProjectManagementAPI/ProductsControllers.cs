f73e9d93b4b6ac42c45ece8fef551bb245e0dad3
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-09-23 22:07:03 +0700

    Init ORM and ApiController

---
+++ ProjectManagementAPI/ProductsControllers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+
+namespace ProjectManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsControllers : ControllerBase
+    {
+        private IProductRepository repository = new ProductRepository();
+
+        //GET: api/Products
+        [HttpGet] 
+    }
+}
